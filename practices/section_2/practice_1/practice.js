b72ce94c76c444c450d3c0c4f074fa14775e731f
OliverYang <dfexpress666@gmail.com>
2018-03-29 13:33:32 +0800

    finish section2

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,39 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+    var object_count = {};
+    var collection_result = [{}];
+    var obj_factor = {};
+    var i = 0, j = 0;
+
+    for (i in collection){
+        if (!object_count[collection[i]]) {
+            object_count[collection[i]] = 1;
+        }
+        else {
+            object_count[collection[i]]++;
+        }
+    }
+    for (i in object_count){
+        obj_factor = {};
+        obj_factor["key"] = i;
+        obj_factor["count"] = object_count[i];
+        collection_result[j] = obj_factor;
+        j++;
+    }
+
+    return collection_result;
 }
 
 module.exports = count_same_elements;
+
+/*var collection = [
+    "a", "a", "a",
+    "e", "e", "e", "e", "e", "e", "e",
+    "h", "h", "h", "h", "h", "h", "h", "h", "h", "h", "h",
+    "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t",
+    "f", "f", "f", "f", "f", "f", "f", "f", "f",
+    "c", "c", "c", "c", "c", "c", "c", "c",
+    "g", "g", "g", "g", "g", "g", "g",
+    "b", "b", "b", "b", "b", "b",
+    "d", "d", "d", "d", "d"
+];
+console.log(count_same_elements(collection));*/
--- practices/section_2/practice_3/practice.js
+++ practices/section_2/practice_3/practice.js
@@ -1,5 +1,35 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+    var object_count = {};
+    var collection_result = [{}];
+    var obj_factor = {};
+    var i = 0, j = 0;
+
+    for (i in collection) {
+        if (collection[i].length > 1) {
+            if (!object_count[collection[i].charAt(0)]) {
+                object_count[collection[i].charAt(0)] = parseInt(collection[i].match(/\d/g).join(""));
+            }
+            else {
+                object_count[collection[i].charAt(0)] += parseInt(collection[i].match(/\d/g).join(""));
+            }
+            continue;
+        }
+        if (!object_count[collection[i]]) {
+            object_count[collection[i]] = 1;
+        }
+        else {
+            object_count[collection[i]]++;
+        }
+    }
+    for (i in object_count){
+        obj_factor = {};
+        obj_factor["name"] = i;
+        obj_factor["summary"] = object_count[i];
+        collection_result[j] = obj_factor;
+        j++;
+    }
+
+    return collection_result;
 }
 
 module.exports = count_same_elements;
