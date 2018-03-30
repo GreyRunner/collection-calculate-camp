9f994f03a9cc574c07c08b72208c60be9b63a946
OliverYang <dfexpress666@gmail.com>
2018-03-30 14:02:42 +0800

    finish flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,21 @@
 'use strict';
 
 function double_to_one(collection) {
+    var collection_result = [];
+    var i = 0, j = 0;
 
-  //在这里写入代码
+    for (i in collection){
+        if (typeof(collection[i]) != "number"){
+            for (j in collection[i]) {
+                collection_result.push(collection[i][j]);
+            }
+        }
+        else {
+            collection_result.push(collection[i]);
+        }
+    }
+
+    return collection_result;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,34 @@
 'use strict';
 
 function double_to_one(collection) {
+    var collection_result = [];
+    var i = 0, j = 0;
 
-  //在这里写入代码
+    for (i in collection){
+        if (typeof(collection[i]) != "number"){
+            for (j in collection[i]) {
+                if (!exist(collection[i][j], collection_result)) {
+                    collection_result.push(collection[i][j]);
+                }
+            }
+        }
+        else {
+            if (!exist(collection[i], collection_result)) {
+                collection_result.push(collection[i]);
+            }
+        }
+    }
+
+    return collection_result;
+}
+
+function exist(number, collection) {
+    for (var i in collection){
+        if (collection[i] == number){
+            return true;
+        }
+    }
+    return false;
 }
 
 module.exports = double_to_one;
