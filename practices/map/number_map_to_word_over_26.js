ba60b4340d8c88c1d27d3f0efe6bf5304660c643
OliverYang <dfexpress666@gmail.com>
2018-03-30 14:48:04 +0800

    finish map

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,12 +1,29 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
+
+
     var collection_result = [];
-    var i;
+    var i = 0;
     var a_ASCII = 'a'.charCodeAt();
+    var letter = '';
 
     for (i in collection){
-
-        collection_result.push(String.fromCharCode(a_ASCII + collection[i] - 1));
+        if (collection[i] > 26) {
+            letter = '';
+            if (collection[i] % 26 == 0){
+                letter += String.fromCharCode(a_ASCII + collection[i] / 26 - 2);
+                letter += 'z';
+                collection_result.push(letter);
+            }
+            else {
+                letter += String.fromCharCode(a_ASCII + collection[i] / 26 - 1);
+                letter += String.fromCharCode(a_ASCII + collection[i] % 26 - 1);
+                collection_result.push(letter);
+            }
+        }
+        else {
+            collection_result.push(String.fromCharCode(a_ASCII + collection[i] - 1));
+        }
     }
 
     return collection_result;
