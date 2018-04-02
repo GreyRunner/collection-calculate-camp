29133f28511fb5868b5dd65be854f220a2d9ca60
OliverYang <dfexpress666@gmail.com>
2018-04-02 11:12:21 +0800

    finish ruduce

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -1,7 +1,15 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+    var collection = [number];
+
+    while (number > 0){
+        number -= interval;
+        number = parseFloat(number.toFixed(1));
+        collection.push(number)
+    }
+
+    return collection;
 }
 
 module.exports = spilt_to_zero;
